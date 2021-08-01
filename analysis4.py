fd9f00e3c895c78ea01a4fcd260c3714439aaa8c
Eeshan Narula <69961210+eeshannarula29@users.noreply.github.com>
2021-08-01 11:08:50 +0530

    change docstring and comments

--- analysis4.py
+++ analysis4.py
@@ -1,34 +1,43 @@
 """
 This file contains one of the many analysis conducted under
 the University Students Entrepreneurial Competency Research.
-The analysis question answered in this file is:
-If a student is influenced, then what matters more: confidence or projects?
-Is this different for STEM and non STEM students?
 
-Conclusion:
-1. When a student is influenced, percentage of students undertaking projects and
+The analysis question answered in this file is:
+
+    If a student is influenced, then what matters more: 
+    confidence or projects? Is this different for STEM 
+    and non STEM students?
+    
+Conclusion in a Nutshell:
+- When a student is influenced, percentage of students undertaking projects and
 having high self-confidence is similar.
-2. When a student is influenced, the percentage of STEM students and non-STEM students
+- When a student is influenced, the percentage of STEM students and non-STEM students
 with high self-confidence is similar.
-3. When a student is influenced, percentage of STEM students undertaking individual projects is slightly
+- When a student is influenced, percentage of STEM students undertaking individual projects is slightly
 higher than non-STEM students.
 
 Note: High self-confidence is defined as a rating of 4 or 5 in self-confidence.
 """
 
-from data_processing import data
+from data_preprocessing import data
 import matplotlib.pyplot as plt
 
-# a) Relation with confidence
+# -- a) Relation of confidence and projects
+
+# Extracting confidence of influenced students
 very_influenced_confidence = data[data['Influenced'] == 1].groupby(['SelfConfidence']).count()['Age']
+
 array_1 = [1, 2, 3, 4, 5]
 array_2 = [very_influenced_confidence[x] for x in array_1]
+
 high_conf = (array_2[3] + array_2[4]) * 100 / sum(array_2)
 
-# b) relation with projects
+# Extracting projects of influenced students
 very_influenced_projects = data[data['Influenced'] == 1].groupby(['IndividualProject']).count()['Age']
+
 project_1 = [0, 1]
 project_2 = [very_influenced_projects[x] for x in project_1]
+
 ind_p = project_2[1] * 100 / sum(project_2)
 
 lis_1 = [high_conf, ind_p]
@@ -41,19 +50,24 @@ plt.bar(lis_2, lis_1, color='maroon',
         width=0.4)
 
 plt.ylabel("Percentage of students")
+
 plt.show()
 
+# -- b) Relation of confidence for STEM and Non STEM students
 
-# c) Relation with confidence for STEM students
-very_influenced_confidence_stem = \
-    data[(data['Influenced'] == 1) & (data["is_stem"] == 1)].groupby(['SelfConfidence']).count()['Age']
+# Extracting confidence for STEM students
+very_influenced_confidence_stem = data[(data['Influenced'] == 1) & 
+                                       (data["is_stem"] == 1)].groupby(['SelfConfidence']).count()['Age']
+
 array_1 = [1, 2, 3, 4, 5]
 array_2 = [very_influenced_confidence_stem[x] for x in array_1]
+
 answer_confidence = (array_2[3] + array_2[4]) * 100 / sum(array_2)
 
-# d) Relation with confidence for non-STEM students
-very_influenced_confidence_non_stem = \
-    data[(data['Influenced'] == 1) & (data["is_stem"] == 0)].groupby(['SelfConfidence']).count()['Age']
+# Extracting confidence for non-STEM students
+very_influenced_confidence_non_stem = data[(data['Influenced'] == 1) &
+                                           (data["is_stem"] == 0)].groupby(['SelfConfidence']).count()['Age']
+
 array_2_non = [very_influenced_confidence_non_stem[x] for x in array_1]
 answer_confidence_non = (array_2_non[3] + array_2_non[4]) * 100 / sum(array_2_non)
 
@@ -68,19 +82,24 @@ plt.bar(lis_2, lis_1, color='maroon',
 
 plt.ylabel("Percentage of students with high self-confidence when influenced")
 plt.xlabel("Degree type of students")
+
 plt.show()
 
+# -- c) Relation of individual projects for STEM and Non STEM students
 
-# e) Relation with individual projects for STEM students
-very_influenced_project_stem = \
-    data[(data['Influenced'] == 1) & (data["is_stem"] == 1)].groupby(['IndividualProject']).count()['Age']
+# Extracting individual projects for STEM students
+very_influenced_project_stem = data[(data['Influenced'] == 1) &
+                                    (data["is_stem"] == 1)].groupby(['IndividualProject']).count()['Age']
+
 array_1_p = [0, 1]
 array_2_p = [very_influenced_project_stem[x] for x in array_1_p]
+
 answer = array_2_p[1] * 100 / sum(array_2_p)
 
-# f) Relation with individual projects for non-STEM students
-very_influenced_projects_non_stem = \
-    data[(data['Influenced'] == 1) & (data["is_stem"] == 0)].groupby(['IndividualProject']).count()['Age']
+# Extracting individual projects for non-STEM students
+very_influenced_projects_non_stem = data[(data['Influenced'] == 1) &
+                                         (data["is_stem"] == 0)].groupby(['IndividualProject']).count()['Age']
+
 array_2_non_p = [very_influenced_projects_non_stem[x] for x in array_1_p]
 answer2 = array_2_non_p[1] * 100 / sum(array_2_non_p)
 
@@ -95,4 +114,5 @@ plt.bar(lis2, lis, color='maroon',
 
 plt.ylabel("Percentage of students undertaking individual projects when influenced")
 plt.xlabel("Degree type of students")
+
 plt.show()
